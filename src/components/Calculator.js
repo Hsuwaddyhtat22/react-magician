5c8a7d567deb2da9f72911fb6448213c52d3df46
Hsu Waddy <hsuwady2001@gmail.com>
2023-09-13 00:00:31 +0630

    Add-component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,212 +1,69 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class Calculator extends Component {
-  constructor() {
-    super();
-    this.state = {
-      input: '',
-      result: '',
-    };
-  }
+function App() {
+  const [input, setInput] = useState('');
 
-    handleButtonClick = (value) => {
-      if (value === '=') {
-        try {
-          const result = eval(this.state.input);
-          this.setState({ result });
-        } catch (error) {
-          this.setState({ result: 'Error' });
-        }
-      } else if (value === 'C') {
-        this.setState({ input: '', result: '' });
-      } else {
-        this.setState((prevState) => ({
-          input: prevState.input + value,
-        }));
+  const handleClick = (value) => {
+    if (value === '=') {
+      try {
+        setInput(eval(input).toString());
+      } catch (error) {
+        setInput('Error');
       }
-    };
-
-    render() {
-      return (
-        <div className="calculator">
-          <div className="display">
-            <input
-              type="text"
-              value={this.state.input}
-              readOnly
-            />
-            <div className="result">
-              {' '}
-              { this.state.result }
-              {' '}
-
-            </div>
-          </div>
-          {' '}
-          <div className="buttons">
-            <div className="row">
-              <button onClick={
-                () => this.handleButtonClick('7')
-            }
-              >
-                {' '}
-                7
-                {' '}
-              </button>
-              {' '}
-              <button onClick={
-                () => this.handleButtonClick('8')
-            }
-              >
-                {' '}
-                8
-                {' '}
-              </button>
-              {' '}
-              <button onClick={
-                () => this.handleButtonClick('9')
-            }
-              >
-                {' '}
-                9
-                {' '}
-              </button>
-              {' '}
-              <button onClick={
-                () => this.handleButtonClick('/')
-            }
-              >
-                {' '}
-                /
-              </button>
-            </div>
-            {' '}
-            <div className="row">
-              <button onClick={
-                () => this.handleButtonClick('4')
-            }
-              >
-                {' '}
-                4
-                {' '}
-              </button>
-              {' '}
-              <button onClick={
-                () => this.handleButtonClick('5')
-            }
-              >
-                {' '}
-                5
-                {' '}
-              </button>
-              {' '}
-              <button onClick={
-                () => this.handleButtonClick('6')
-            }
-              >
-                {' '}
-                6
-                {' '}
-              </button>
-              {' '}
-              <button onClick={
-                () => this.handleButtonClick('*')
-            }
-              >
-                {' '}
-                *
-                {' '}
-              </button>
-              {' '}
-
-            </div>
-            {' '}
-            <div className="row">
-              <button onClick={
-                () => this.handleButtonClick('1')
-            }
-              >
-                {' '}
-                1
-                {' '}
-              </button>
-              {' '}
-              <button onClick={
-                () => this.handleButtonClick('2')
-            }
-              >
-                {' '}
-                2
-                {' '}
-              </button>
-              {' '}
-              <button onClick={
-                () => this.handleButtonClick('3')
-            }
-              >
-                {' '}
-                3
-                {' '}
-              </button>
-              {' '}
-              <button onClick={
-                () => this.handleButtonClick('-')
-            }
-              >
-                {' '}
-                -
-                {' '}
-              </button>
-              {' '}
-
-            </div>
-            {' '}
-            <div className="row">
-              <button onClick={
-                () => this.handleButtonClick('C')
-            }
-              >
-                {' '}
-                C
-                {' '}
-              </button>
-              {' '}
-              <button onClick={
-                () => this.handleButtonClick('0')
-            }
-              >
-                {' '}
-                0
-                {' '}
-              </button>
-              {' '}
-              <button onClick={
-                () => this.handleButtonClick('=')
-            }
-              >
-                =
-                {' '}
-              </button>
-              {' '}
-              <button onClick={
-                () => this.handleButtonClick('+')
-            }
-              >
-                {' '}
-                +
-                {' '}
-              </button>
-              {' '}
-
-            </div>
-            {' '}
-
-          </div>
-          {' '}
-
-        </div>
-      );
+    } else if (value === 'C') {
+      setInput('');
+    } else if (value === 'AC') {
+      setInput('');
+    } else if (value === '+/-') {
+      setInput((prevInput) => (prevInput.charAt(0) === '-' ? prevInput.slice(1) : `-${prevInput}`));
+    } else if (value === 'Del') {
+      setInput((prevInput) => prevInput.slice(0, -1));
+    } else {
+      setInput(input + value);
     }
+  };
+
+  const buttons = [
+    'AC', '+/-', '%', '/',
+    '7', '8', '9', '*',
+    '4', '5', '6', '-',
+    '1', '2', '3', '+',
+    '0', '.', '=', 'Del',
+  ];
+
+  return (
+    <div className="calculator">
+      <div className="input">
+        {' '}
+        { input }
+        {' '}
+
+      </div>
+      {' '}
+      <div className="buttons">
+        {' '}
+        {
+            buttons.map((button) => (
+              <button
+                key={button}
+                onClick={
+                    () => handleClick(button)
+                }
+              >
+                {' '}
+                { button }
+                {' '}
+
+              </button>
+            ))
+        }
+        {' '}
+
+      </div>
+      {' '}
+
+    </div>
+  );
 }
 
-export default Calculator;
+export default App;
